8616c0926fe938d7222689848133f7bd0bdca1f1
Cassio M. M. Pereira <cassiomartini@gmail.com>
2014-03-06 09:33:58 -0300

    removing code duplication

--- src/main/java/br/fapesp/myutils/MyUtils.java
+++ src/main/java/br/fapesp/myutils/MyUtils.java
@@ -145,25 +145,8 @@ public class MyUtils {
 	 * @return a 2-d array listing the edges of the MST.
 	 */
 	public static int[][] fastPrim(double[][] data) {	
-		int N = data.length;		
-		int[][] mst = new int[N-1][2];
 		double[][] D = getEuclideanMatrix(data); // euclidean dissimilarity matrix
-		HeapKey[] keys = new HeapKey[N-1];
-		
-		for (int i = 1; i < N; i++)
-			keys[i - 1] = new EdgeElement(i, 0, D[0][i]);
-		
-		MinHeap mh = new MinHeap(keys);
-		mh.build();
-		
-		for (int i = 0; i < N - 1; i++) {
-			EdgeElement e = (EdgeElement) mh.getMinFast();
-			mst[i][0] = e.v;
-			mst[i][1] = e.u;
-			mh.updateKeys(e.u, D);
-		}
-			
-		return mst;
+		return(fastPrim(data, D));
 	}
 	
 	/**
